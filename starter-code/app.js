df3f058405045f5ed2aa2bb90f0882da66a7557e
Kevin Couture <kcouture939@gmail.com>
2019-06-07 13:44:45 -0700

    Completed sumArray.

--- starter-code/app.js
+++ starter-code/app.js
@@ -86,12 +86,18 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  var total = 0;
+  for(var i = 0; i < sumArr.length; i++) {
+    total += sumArr[i];
+  }
+  var strBuild = sumArr + ' was passed in as an array of numbers, and ' + total + ' is their sum.';
+  return [total, strBuild];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+// eslint-disable-next-line no-undef
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
