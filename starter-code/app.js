1d5f8777f91eb0d45cb85c0d05b6908ad5f371db
Kevin Couture <kcouture939@gmail.com>
2019-06-07 13:56:44 -0700

    Completed multArray

--- starter-code/app.js
+++ starter-code/app.js
@@ -113,11 +113,22 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+  var strBuild = 'The numbers ' + multArr + ' have a product of ';
+  var total = 1;
+  // Check for empty array
+  if(multArr.length === 0)
+    total = 0;
+  // Iterate throguh array
+  for(var i = 0; i < multArr.length; i++) {
+    total *= multArr[i];
+  }
+  strBuild += total + '.';
+  return [total, strBuild];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+// eslint-disable-next-line no-undef
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
